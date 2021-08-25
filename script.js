cd481c1dfbddefba0b8136f4c993bb9b11e708a8
Artur <arturduartemoraes@gmail.com>
2021-08-25 10:20:02 -0300

    Lâmpada quebrando com clique duplo

--- script.js
+++ script.js
@@ -1,18 +1,67 @@
 "use strict"
 
+// function botoesLigaDesliga(ligarEstado, desligarEstado) {
+//     const ligar = document.getElementById("ligar")
+//     const desligar = document.getElementById("desligar")
+//     ligar.disabled = ligarEstado
+//     desligar.disabled = desligarEstado
+// }
+
+function lampadaQuebrada() {
+    const lampada = document.getElementById("lampada");
+    return lampada.src.indexOf("quebrada") !== -1;
+}
+
 function ligarLampada() {
     const lampada = document.getElementById('lampada');
-    lampada.src = 'img/ligada.jpg';
+    const botaoLigar = document.getElementById("ligar");
+    const botaoDesligar = document.getElementById("desligar");
+
+    if (!lampadaQuebrada()){
+        lampada.src = 'img/ligada.jpg';
+        botaoLigar.disabled = true;
+        botaoDesligar.disabled = false;
+    }
+    
 }
 
 function desligarLampada() {
     const lampada = document.getElementById('lampada');
-    lampada.src = 'img/desligada.jpg';
+    const botaoLigar = document.getElementById("ligar");
+    const botaoDesligar = document.getElementById("desligar");
+
+    if (!lampadaQuebrada()){
+        lampada.src = 'img/desligada.jpg';
+        botaoDesligar.disabled = true;
+        botaoLigar.disabled = false;
+    }
+}
+
+function quebrarLampada() {
+    const lampada = document.getElementById("lampada");
+    const botaoLigar = document.getElementById("ligar");
+    const botaoDesligar = document.getElementById("desligar");
+
+    lampada.src = "img/quebrada.jpg";
+    botaoLigar.disabled = true;
+    botaoDesligar.disabled = true;
 }
 
 //Eventos
 document.getElementById("ligar")
     .addEventListener("click", ligarLampada)
 
-    document.getElementById("desligar")
+document.getElementById("desligar")
     .addEventListener("click", desligarLampada)
+
+document.getElementById("lampada")
+    .addEventListener("mouseover", ligarLampada)
+
+document.getElementById("lampada")
+    .addEventListener("mouseleave", desligarLampada)
+
+
+document.getElementById("lampada")
+    .addEventListener("dblclick", quebrarLampada)
+// document.getElementById("desligar")
+//     .addEventListener("click", desligarLampada)
